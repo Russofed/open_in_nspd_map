8bb932682381bd2176930fc1eaf2a46fb7380acb
Russofed <132661975+Russofed@users.noreply.github.com>
2025-04-29 09:11:03 +0500

    Версия 2.0
    1 Добавлена возможно выбора открытия браузера по умолчанию
    2 Добавлена функция выбора слоев для отображения на портале NSPD

--- open_in_nspd_map.py
+++ open_in_nspd_map.py
@@ -5,36 +5,91 @@
                                  A QGIS plugin
  Opens the current map extent in NSPD map
                               -------------------
-        begin                : 2024-12-17
-        copyright            : (C) 2024 Harry King
-        email                : nlu1vwm0@anonaddy.com
-        git sha              : $Format:%H$
+        begin                : 2025-04-28
+        copyright            : (C) 2025 Alexandr_Fedorov
+        email                : rusorur@mail.ru
  ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-
-###
-
-Core plugin code
-
-###
 """
 
-# Import Packages - QGIS
 from qgis.PyQt.QtGui import QIcon
-from qgis.PyQt.QtWidgets import QAction
+from qgis.PyQt.QtWidgets import (QAction, QFileDialog, QMessageBox, 
+                                QDialog, QVBoxLayout, QCheckBox, QPushButton)
+from qgis.PyQt.QtCore import QSettings
 from qgis.core import QgsProject, QgsCoordinateReferenceSystem, QgsCoordinateTransform, Qgis
-
-# Import Packages - General
 import os
-import webbrowser
+import subprocess
+
+class LayerSettingsDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Отображение слоев для охвата на сайте NSPD")
+        self.layout = QVBoxLayout()
+        
+        # Список слоёв и их ID
+        self.layers = {
+            "Кадастровые округа": 36945,
+            "Кадастровые районы": 36070,
+            "Кадастровые кварталы": 36071,
+            "Государственная граница Российской Федерации": 37313,
+            "Субъекты Российской Федерации (линии)": 37314,
+            "Субъекты Российской Федерации (полигоны)": 37315,
+            "Муниципальные образования (полигональный)": 36278,
+            "Муниципальные образования (линейный)": 36279,
+            "Населённые пункты (полигоны)": 36281,
+            "Населённые пункты (линии)": 37316,
+            "Земельные участки из ЕГРН": 36048,
+            "Земельные участки, образуемые по схеме расположения земельного участка": 37294,
+            "Земельные участки, выставленные на аукцион": 37299,
+            "Земельные участки, свободные от прав третьих лиц": 37298,
+            "Земля для стройки ПКК": 849407,
+            "Земля для туризма ПКК": 849453,
+            "Земельные участки, образуемые по проекту межевания территории": 36473,
+            "Здания": 36049,
+            "Сооружения": 36328,
+            "Объекты незавершенного строительства": 36329,
+            "Единые недвижимые комплексы": 37433,
+            "Предприятие как имущественный комплекс": 37434,
+            "ЗОУИТ объектов культурного наследия": 37577,
+            "ЗОУИТ объектов энергетики, связи, транспорта": 37578,
+            "ЗОУИТ природных территорий": 37580,
+            "ЗОУИТ охраняемых объектов и безопасности": 37579,
+            "Иные ЗОУИТ": 37581,
+            "Территориальные зоны": 36315,
+            "Красные линии": 37293,
+            "Береговые линии (границы водных объектов) (полигональный)": 36469,
+            "Береговые линии (границы водных объектов) (линейный)": 36470,
+        }
+        
+        self.checkboxes = {}
+        
+        # Загружаем сохранённые настройки
+        self.settings = QSettings()
+        self.active_layers = self.settings.value("OpenNSPDMap/ActiveLayers", [])
+        if not self.active_layers:
+            self.active_layers = []
+        
+        # Создаём чекбоксы для каждого слоя
+        for name, layer_id in self.layers.items():
+            cb = QCheckBox(name)
+            cb.setChecked(str(layer_id) in self.active_layers)
+            self.checkboxes[layer_id] = cb
+            self.layout.addWidget(cb)
+        
+        # Кнопка сохранения
+        btn_save = QPushButton("Сохранить")
+        btn_save.clicked.connect(self.save_settings)
+        self.layout.addWidget(btn_save)
+        
+        self.setLayout(self.layout)
+    
+    def save_settings(self):
+        active_layers = []
+        for layer_id, cb in self.checkboxes.items():
+            if cb.isChecked():
+                active_layers.append(str(layer_id))
+        
+        self.settings.setValue("OpenNSPDMap/ActiveLayers", active_layers)
+        self.accept()
 
 class OpenInNSPDMapPlugin:
     def __init__(self, iface):
@@ -44,11 +99,26 @@ class OpenInNSPDMapPlugin:
 
     def initGui(self):
         icon_path = os.path.join(self.plugin_dir, 'icon.png')
-        self.action = QAction(QIcon(icon_path), 'Open in NSPD Map', self.iface.mainWindow())
-        self.action.triggered.connect(self.run)
+        settings_icon_path = os.path.join(self.plugin_dir, 'iconsetting.png')
+        layers_icon_path = os.path.join(self.plugin_dir, 'iconlayers.png')
         
-        self.iface.addToolBarIcon(self.action)
-        self.actions.append(self.action)
+        # Кнопка "Открыть в NSPD Map"
+        self.open_action = QAction(QIcon(icon_path), 'Открыть в NSPD Map', self.iface.mainWindow())
+        self.open_action.triggered.connect(self.run)
+        self.iface.addToolBarIcon(self.open_action)
+        self.actions.append(self.open_action)
+
+        # Кнопка "Настройки слоёв" (вторая)
+        self.layers_action = QAction(QIcon(layers_icon_path), 'Отображение слоев для охвата на сайте NSPD', self.iface.mainWindow())
+        self.layers_action.triggered.connect(self.open_layers_dialog)
+        self.iface.addToolBarIcon(self.layers_action)
+        self.actions.append(self.layers_action)
+
+        # Кнопка "Настройки браузера" (третья)
+        self.settings_action = QAction(QIcon(settings_icon_path), 'Настройки браузера', self.iface.mainWindow())
+        self.settings_action.triggered.connect(self.open_settings_dialog)
+        self.iface.addToolBarIcon(self.settings_action)
+        self.actions.append(self.settings_action)
 
     def unload(self):
         for action in self.actions:
@@ -63,19 +133,42 @@ class OpenInNSPDMapPlugin:
         target_crs = QgsCoordinateReferenceSystem('EPSG:3857')
         transform = QgsCoordinateTransform(current_crs, target_crs, QgsProject.instance())
         transformed_extent = transform.transformBoundingBox(extent)
-        
+
         center_x = (transformed_extent.xMinimum() + transformed_extent.xMaximum()) / 2
         center_y = (transformed_extent.yMinimum() + transformed_extent.yMaximum()) / 2
         zoom = self._calculate_zoom_level()
-        
-        nspd_url = f"https://nspd.gov.ru/map?zoom={zoom}&coordinate_x={center_x}&coordinate_y={center_y}&theme_id=1&is_copy_url=true"
-        
-        webbrowser.open(nspd_url)
+
+        # Получаем активные слои из настроек
+        settings = QSettings()
+        active_layers = settings.value("OpenNSPDMap/ActiveLayers", [])
+        layers_param = "%2C".join(active_layers) if active_layers else ""
+
+        nspd_url = (
+            f"https://nspd.gov.ru/map?"
+            f"zoom={zoom}&coordinate_x={center_x}&coordinate_y={center_y}"
+            f"&theme_id=1&is_copy_url=true&active_layers={layers_param}"
+        )
+
+        browser_path = settings.value("OpenNSPDMap/YandexPath", type=str)
+
+        if browser_path and os.path.exists(browser_path):
+            try:
+                subprocess.Popen([browser_path, nspd_url])
+            except Exception as e:
+                self.iface.messageBar().pushMessage(
+                    "Ошибка запуска браузера", str(e), level=Qgis.Critical
+                )
+        else:
+            self.iface.messageBar().pushMessage(
+                "Не задан путь к Yandex Browser",
+                "Перейдите в настройки плагина и укажите путь к Yandex.Браузеру",
+                level=Qgis.Warning
+            )
 
     def _calculate_zoom_level(self):
         canvas = self.iface.mapCanvas()
         scale = canvas.scale()
-        
+
         if scale < 100: return 20
         elif scale < 250: return 19
         elif scale < 500: return 18
@@ -95,3 +188,26 @@ class OpenInNSPDMapPlugin:
         elif scale < 25000000: return 4
         elif scale < 50000000: return 3
         else: return 2
+
+    def open_settings_dialog(self):
+        settings = QSettings()
+        current_path = settings.value("OpenNSPDMap/YandexPath", "")
+
+        file_path, _ = QFileDialog.getOpenFileName(
+            self.iface.mainWindow(),
+            "Укажите путь к Yandex Browser",
+            current_path or "",
+            "Исполняемые файлы (*.exe);;Все файлы (*)"
+        )
+
+        if file_path:
+            settings.setValue("OpenNSPDMap/YandexPath", file_path)
+            QMessageBox.information(
+                self.iface.mainWindow(),
+                "Настройки сохранены",
+                "Путь к Yandex.Браузеру успешно сохранён."
+            )
+    
+    def open_layers_dialog(self):
+        dialog = LayerSettingsDialog(self.iface.mainWindow())
+        dialog.exec_()
